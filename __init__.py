40ce818e851d700281d58790289e7f87636afe14
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-12-18 13:44:16 +0900

    wip

--- __init__.py
+++ __init__.py
@@ -105,6 +105,8 @@ from impact.util_nodes import *
 from impact.segs_nodes import *
 from impact.special_samplers import *
 from impact.hf_nodes import *
+from impact.bridge_nodes import *
+
 import threading
 
 wildcard_path = impact.config.get_config()['custom_wildcards']
@@ -225,6 +227,7 @@ NODE_CLASS_MAPPINGS = {
     "TwoAdvancedSamplersForMask": TwoAdvancedSamplersForMask,
 
     "PreviewBridge": PreviewBridge,
+    "PreviewLatentBridge": PreviewLatentBridge,
     "ImageSender": ImageSender,
     "ImageReceiver": ImageReceiver,
     "LatentSender": LatentSender,
